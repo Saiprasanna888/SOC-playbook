6e26699ba8e547687e14fdad7b26d51ca5e6ca09
[dyad] <muppallasaiprasanna@gmail.com>
2025-10-18 02:26:40 +0530

    [dyad] Removed the 'Made with Dyad' link from the application footer. - wrote 1 file(s)

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -26,14 +26,6 @@ export const MadeWithDyad = () => {
           <Linkedin className="w-4 h-4" />
         </a>
       </div>
-      <a
-        href="https://www.dyad.sh/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
-      >
-        (Made with Dyad)
-      </a>
     </div>
   );
 };
